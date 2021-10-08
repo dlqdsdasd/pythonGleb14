ebbdd89e89eb6e72bcf43e6eb71e91d5cfa78330
vr <glebab125net@gmail.com>
2021-10-08 18:40:42 +0300

    aaa

--- app.py
+++ app.py
@@ -5,9 +5,29 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///result.db'
 db = SQLAlchemy(app)
 
-@app.route('/news')
-def index():
-    return render_template('index.html')
+
+class News(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    zag = db.Column(db.String(128), nullable=False)
+    tex = db.Column(db.Text, nullable=False)
+
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == 'POST':
+        a = request.form.get('Tem')
+        b = request.form.get('Mes')
+        c = News(zag=a, tex=b)
+        db.session.add(c)
+        db.session.commit()
+        return redirect('/view')
+    return render_template('write.html')
+
+
+@app.route('/view')
+def view():
+    a = News.query.order_by(News.id).all()
+    return render_template('view.html', a=a)
 
 
 app.run(host='127.0.0.1', debug=True)
