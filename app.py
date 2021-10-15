dbc40d24d666ca8e64d51a1dbafacc634d65db90
vr <glebab125net@gmail.com>
2021-10-15 18:46:57 +0300

    aaa

--- app.py
+++ app.py
@@ -10,17 +10,25 @@ class News(db.Model):
     id = db.Column (db.Integer, primary_key=True)
     zag = db.Column(db.String(128), nullable=False)
     tex = db.Column(db.Text, nullable=False)
+    fl = db.Column(db.String(128), nullable=False)
 
 
 @app.route('/write', methods=['GET', 'POST'])
 def write():
     if request.method == 'POST':
-        a = request.form.get('Tem')
-        b = request.form.get('Mes')
-        c = News(zag=a, tex=b)
+        zag = request.form.get('Tem')
+        tex = request.form.get('Mes')
+        dt = request.form.get('dt')
+        fl =  request.form.get('fl')
+        c = News(zag=zag, tex=tex, dt=dt)
         db.session.add(c)
         db.session.commit()
-        return redirect('/view')
+        if fl:
+            r = fl.filename.split('.')[-1]
+            if r == 'jpg' or r == 'jpeg' or r == 'png':
+                filename1 =
+
+        return redirect('/')
     return render_template('write.html')
 
 
